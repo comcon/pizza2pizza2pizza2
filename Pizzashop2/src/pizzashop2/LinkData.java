a90ef70f05d12cd868e73f9418f59b8226d6b7a4
g12929mk <g12929mk@s30419.tsuda.ac.jp>
2014-06-06 10:42:58 +0900

    ピザショップです

---
+++ Pizzashop2/src/pizzashop2/LinkData.java
@@ -0,0 +1,60 @@
+package pizzashop2;
+ 
+import java.util.Date;
+ 
+import javax.jdo.annotations.*;
+ 
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class LinkData {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Long id;
+     
+    @Persistent
+    private String title;
+     
+    @Persistent
+    private String url;
+     
+    @Persistent
+    private Date datetime;
+ 
+    public LinkData(String title, String url, Date datetime) {
+        super();
+        this.title = title;
+        this.url = url;
+        this.datetime = datetime;
+    }
+ 
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+ 
+    public String getUrl() {
+        return url;
+    }
+ 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+ 
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+}
